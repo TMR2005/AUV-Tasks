84f71bf287489a79807cccdbfd8a42b368b4ce86
Praveen Kumar <148249199+TMR2005@users.noreply.github.com>
2025-04-18 23:47:13 +0530

    Add files via upload

---
+++ Question1/chat_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/vboxuser/chat_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/vboxuser/chat_ws/devel;/home/vboxuser/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/vboxuser/chat_ws/devel;/home/vboxuser/catkin_ws/devel;/opt/ros/noetic' != '' else []
