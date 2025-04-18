c7200fd4ecb3d05afe76b9d31c7b4ffbe76752f0
Praveen Kumar <148249199+TMR2005@users.noreply.github.com>
2025-04-19 00:47:40 +0530

    Add files via upload

---
+++ Question2/qn2_ws/build/calc/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/vboxuser/qn2_ws/src/calc
+# Build directory: /home/vboxuser/qn2_ws/build/calc
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
