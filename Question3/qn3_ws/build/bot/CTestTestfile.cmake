eb468c050291aafacd210340c8e1b9b82e358dbd
Praveen Kumar <148249199+TMR2005@users.noreply.github.com>
2025-04-19 09:51:33 +0530

    Add files via upload

---
+++ Question3/qn3_ws/build/bot/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/vboxuser/qn3_ws/src/bot
+# Build directory: /home/vboxuser/qn3_ws/build/bot
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ Question3/qn3_ws/build/bot/catkin_generated/installspace/bot-msg-extras.cmake
@@ -0,0 +1,2 @@
+set(bot_MESSAGE_FILES "msg/BotPose.msg")
+set(bot_SERVICE_FILES "")
