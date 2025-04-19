14165dc4cd2121a3b3bf3c91321c3525df419659
Praveen Kumar <148249199+TMR2005@users.noreply.github.com>
2025-04-19 09:47:49 +0530

    Add files via upload

---
+++ Question3/qn3_ws/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/vboxuser/qn3_ws/src
+# Build directory: /home/vboxuser/qn3_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("bot")
